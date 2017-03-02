d9b464443e85f3bc5b68b3a78f3e9f5a606b0af9
MaguitaSlzr <maguita.01s@gmail.com>
2017-03-02 00:14:50 -0400

    Se cambio colores de fondo, base, login, menu.

--- application/views/base/base_view.php
+++ application/views/base/base_view.php
@@ -1,12 +1,12 @@
-<h3 class="tituloPrincipal">SISTEMA ACADEMICO<br/>MENU PRINCIPAL</h3>
+<h3 class="tituloPrincipal">SISTEMA  ACADEMICO<br/>MENU PRINCIPAL</h3>
 <!--
 <p>
   Bienvenido <strong><?php echo $this->session->userdata("usuario")?></strong>.
 </p>
 -->
 <div class="menu-principal" style="text-align:center;">
-  <a href="<?php echo site_url('estudiante/registro_estudiantes');?>"><img src="<?php echo base_url();?>resources/base/img/registro_estudiantes.gif" class="img-rounded" alt="Responsive image" style="margin-right:40px;margin-bottom:20px;"></a>
-  <a href="<?php echo site_url('estudiante/asignar_curso');?>"><img src="<?php echo base_url();?>resources/base/img/asigna_curso_estudiante.gif" class="img-rounded" alt="Responsive image" style="margin-bottom:20px;"></a>
-  <a href="<?php echo site_url('estudiante/form_registro_disciplinario');?>"><img src="<?php echo base_url();?>resources/base/img/control_disciplina.gif" class="img-rounded" alt="Responsive image" style="margin-right:40px;margin-bottom:20px;"></a>
- <!-- <a href="<?php echo site_url('estudiante/form_control_asistencia');?>"><img src="<?php echo base_url();?>resources/base/img/control_asistencia.gif" class="img-rounded" alt="Responsive image" style="margin-bottom:20px;"></a>-->
+  <a href="<?php echo site_url('estudiante/registro_estudiantes');?>"><img src="<?php echo base_url();?>resources/base/img/registro_estudiantes.bmp" class="img-rounded" alt="Responsive image" style="margin-right:40px;margin-bottom:20px;"></a>
+  <a href="<?php echo site_url('estudiante/asignar_curso');?>"><img src="<?php echo base_url();?>resources/base/img/asigna_curso_estudiante.bmp" class="img-rounded" alt="Responsive image" style="margin-bottom:20px;"></a>
+  <a href="<?php echo site_url('estudiante/form_registro_disciplinario');?>"><img src="<?php echo base_url();?>resources/base/img/control_disciplina.bmp" class="img-rounded" alt="Responsive image" style="margin-right:40px;margin-bottom:20px;"></a>
+  <a href="<?php echo site_url('estudiante/form_control_asistencia');?>"><img src="<?php echo base_url();?>resources/base/img/reportes.bmp" class="img-rounded" alt="Responsive image" style="margin-bottom:20px;"></a>
 </div>
--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -5,75 +5,75 @@
             <div class="container-fluid">
                 <div class="navbar-header">
                     <div class="contLogoSystem">
-                    <img width="70" height="70" class="img-responsive imgSystem" alt="" src="<?php echo base_url();?>resources/base/img/escudo_oficial.png">
-                </div>
+                        <img width="70" height="70" class="img-responsive imgSystem" alt="" src="<?php echo base_url(); ?>resources/base/img/escudo_oficial.png">
+                    </div>
                     <div class="barra">
                         <button class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                       <a href="<?php echo site_url('login/index');?>" class="navbar-brand">INICIO</a>
-            </div>
-            </div>
+                        <a href="<?php echo site_url('login/index'); ?>" class="navbar-brand">INICIO</a>
+                    </div>
+                </div>
 
                 <div class="collapse navbar-collapse" id="navbar-1">
-                <ul class="nav navbar-nav">
+                    <ul class="nav navbar-nav">
                         <!--<li class="dropdown">
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Institución<span class="caret"></span></a>
                           <ul class="dropdown-menu">
                             <li><a href="#">Gestión de Cursos</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="<?php //echo site_url('institucion/crud_faltas'); ?>">Gestión Faltas</a></li>
+                            <li><a href="<?php //echo site_url('institucion/crud_faltas');  ?>">Gestión Faltas</a></li>
                             <li><a href="#">Gestión Diciplinario</a></li>
                           </ul>
                         </li>-->
                         <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Docentes<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-briefcase"></span> Docentes<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
                                 <li><a href="<?php echo site_url('docente/registro_docentes'); ?>">Registro de Docentes</a></li>
-                                <!--<li><a href="<?php //echo site_url('docente/asignacion_academica'); ?>">Asignación Docente Horario</a></li>-->
+                                <!--<li><a href="<?php //echo site_url('docente/asignacion_academica');  ?>">Asignación Docente Horario</a></li>-->
                                 <!--<li><a href="#">Reporte Docentes</a></li>-->
                             </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Estudiantes<span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-education"></span> Estudiantes<span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <!--
-                                  <li><a href="<?php //echo site_url('estudiante/base_view'); ?>">Matricula Estudiantes</a></li>
+                                  <li><a href="<?php //echo site_url('estudiante/base_view');  ?>">Matricula Estudiantes</a></li>
                                 -->
                                 <li><a href="<?php echo site_url('estudiante/registro_estudiantes'); ?>">Registro de Estudiantes</a></li>  
                                 <li><a href="<?php echo site_url('estudiante/asignar_curso'); ?>">Asignación Estudiante Curso</a></li>
-                                <!--<li><a href="#">Registro Academico</a></li>-->
-                                <li><a href="<?php echo site_url('estudiante/form_control_asistencia'); ?>">Registro de Asistencia</a></li>
+                                <!--<li><a href="#">Registro Academico</a></li>-->                                
                             </ul>
                         </li>
                         <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Registro Disciplinario<span class="caret"></span></a>
-                      <ul class="dropdown-menu">                              
-                                <!--<li><a href="//<php echo site_url('estudiante/registro_disciplinario'); ?>">Control Disciplinario</a></li>-->
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-blackboard"></span> Registro Disciplinario<span class="caret"></span></a>
+                            <ul class="dropdown-menu">                              
+                                      <!--<li><a href="//<php echo site_url('estudiante/registro_disciplinario'); ?>">Control Disciplinario</a></li>-->
+                                <li><a href="<?php echo site_url('estudiante/form_control_asistencia'); ?>">Registro de Asistencia</a></li>
                                 <li><a href="<?php echo site_url('estudiante/form_registro_disciplinario'); ?>">Registro control Disciplinario</a></li>
                                 <li><a href="<?php echo site_url('estudiante/printReporteDisciplinario'); ?>" target="_blank">Resumen Disciplinario</a></li>
                             </ul>
                         </li>
                         <li class="dropdown">
-                           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reportes<span class="caret"></span></a>
-                           <ul class="dropdown-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-list-alt"></span> Reportes<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
                                 <li><a href="<?php echo site_url('estudiante/form_reporte_general'); ?>">Reporte por Estudiante</a></li>
-                             <li><a href="<?php// echo site_url('estudiante/printReporteDisciplinario'); ?>" target="_blank">Reporte General</a></li>
-                             <li><a href="<?php //echo site_url('estudiante/printReporteListado'); ?>" target="_blank">Reporte por curso</a></li>
-                           </ul>
-                         </li>
+                                <li><a href="<?php // echo site_url('estudiante/printReporteDisciplinario');  ?>" target="_blank">Reporte General</a></li>
+                                <li><a href="<?php //echo site_url('estudiante/printReporteListado');  ?>" target="_blank">Reporte por curso</a></li>
+                            </ul>
+                        </li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
-                       <!-- <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span> Configuración <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Usuarios</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="<?php //echo site_url('configuracion/backup'); ?>"><span class="glyphicon glyphicon-hdd"></span> Copia de Seguridad</a></li>
-                            </ul>
-                        </li>-->
+                        <!-- <li class="dropdown">
+                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span> Configuración <span class="caret"></span></a>
+                             <ul class="dropdown-menu">
+                                 <li><a href="#"><span class="glyphicon glyphicon-user"></span> Usuarios</a></li>
+                                 <li role="separator" class="divider"></li>
+                                 <li><a href="<?php //echo site_url('configuracion/backup');  ?>"><span class="glyphicon glyphicon-hdd"></span> Copia de Seguridad</a></li>
+                             </ul>
+                         </li>-->
                         <li><a href="#"><span class="glyphicon glyphicon-user"></span> <?php echo $this->session->userdata("paterno"); ?></a></li>
                         <li><a href="<?php echo site_url('login/cerrar'); ?>"><span class="glyphicon glyphicon-off"></span> Cerrar Sesion</a></li>
                     </ul>             
